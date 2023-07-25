873972841679fc710e7b318cfe9dbc5c33ccc545
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2023-07-25 23:11:40 +0530

    create label and input component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import './App.css';
 function MyButton(){
   return(
     <button>
-        myButton
+        submit
     </button>
   );
+}
+
+function MyLabel(){
+  return(
+    <label for="name">Name</label>
+  )
+}
+
+function MyInput(){
+  return(
+    <input id='name' placeholder='name'></input>
+  )
 }
 export default function App() {
   return (
     <div className="App">
+      <MyLabel/>
+      <MyInput/>
       <MyButton/>
     </div>
   );
